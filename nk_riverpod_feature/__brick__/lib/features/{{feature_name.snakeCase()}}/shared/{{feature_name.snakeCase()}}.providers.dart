cc456222644d1c4d62ae297e2cf295c6b2501f56
Nico Klage <nico.klage@gmx.de>
2024-08-27 21:55:54 +0200

    feat: makefile support for localizations

--- nk_riverpod_feature/__brick__/lib/features/{{feature_name.snakeCase()}}/shared/{{feature_name.snakeCase()}}.providers.dart
+++ nk_riverpod_feature/__brick__/lib/features/{{feature_name.snakeCase()}}/shared/{{feature_name.snakeCase()}}.providers.dart
@@ -3,9 +3,9 @@ import 'dart:ui';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../l10n/{{feature_name.snakeCase()}}_localization.dart';
 import '../data.dart';
 import '../domain.dart';
+import '../l10n/{{feature_name.snakeCase()}}_localization.dart';
 import '../presentation.dart';
 
 /// {{feature_name.pascalCase()}} Feature Providers
